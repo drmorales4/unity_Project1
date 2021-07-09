db3e5af87c02013e0c6e84eeedb1df33575f0c7d
drmorales4 <davidmoralesec@gmail.com>
2021-07-09 18:44:12 -0500

    tutorial 4

--- Assets/CuboDataController.cs
+++ Assets/CuboDataController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Video;
 
 public class CuboDataController : MonoBehaviour, IAction
 {
@@ -9,10 +10,24 @@ public class CuboDataController : MonoBehaviour, IAction
     [SerializeField] public int numStand;
     [SerializeField] Text uiText;
     [SerializeField] Controller controller;
+    [SerializeField] VideoPlayer videoPlayer;
+    [SerializeField] GameObject btnVideo;
 
     public void Activate()
     {
         uiText.text = controller.data.stand[numStand].title;
+
+
+        if (!controller.data.stand[numStand].video.Equals(""))
+        {
+            videoPlayer.url = controller.data.stand[numStand].video;
+            btnVideo.SetActive(true);
+        }
+        else
+        {
+            btnVideo.SetActive(false);
+        }
+        
     }
 
     // Start is called before the first frame update
